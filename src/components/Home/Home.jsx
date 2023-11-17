9d678b14dcb68e4f8bd4a92d6e47a1f391499599
Bill Cathey <billcathey@hotmail.com>
2023-11-16 20:39:01 -0500

    Implement data fetching in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
-const Home = () => (
-    <div className="home">
-        <h2>Home</h2>
-    </div>
-)
+import { useEffect } from "react"
+import axios from 'axios'
+
+const Home = () => {
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const result = await axios(
+                'http://localhost:8000/movies/'
+            )
+            .catch((err) => {
+                console.log("Error:", err)
+            })
+            console.log(result.data)
+        }
+        fetchMovies()
+    }, [])
+
+    return (
+        <div className="home">
+            <h2>Home</h2>
+        </div>
+    )
+}
 
 export default Home
