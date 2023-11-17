1575c85d803dda4a5e274b5192082d245cfbb511
Bill Cathey <billcathey@hotmail.com>
2023-11-16 23:09:53 -0500

    Edit MovieList title

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -11,7 +11,7 @@ const MovieList = () => {
 
     return (
         <>
-            <h2>Movie List</h2>
+            <h2>Top Movies</h2>
             <div className="movie-container">{renderedMovies}</div>
         </>
     )
