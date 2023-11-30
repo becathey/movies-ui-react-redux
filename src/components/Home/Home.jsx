fe89bb72b03041366842a22886c111ec0519a422
Bill Cathey <billcathey@hotmail.com>
2023-11-29 21:23:03 -0500

    Correct case of directory in import statement in Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { fetchAsyncMovies } from "../../features/movies/movieSlice"
-import MovieList from "../movieList/MovieList"
+import MovieList from "../MovieList/MovieList"
 
 const Home = () => {
     const dispatch = useDispatch()
